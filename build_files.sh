8beaf12be292f75f8483b1ed3fc380371ff41092
Sudhir Kumar <sudhirkdnw@gmail.com>
2024-09-13 22:10:26 +0530

    finallll

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,13 @@
+#!/bin/bash
 
 echo "BUILD START"
 
-# Install dependencies
 pip install -r requirements.txt
 
-# Run Django management commands
 python manage.py makemigrations
 python manage.py migrate
 python manage.py tailwind install
-
-# Collect static files
-python manage.py collectstatic --noinput
-
-# Start tailwind (if needed, otherwise remove this line)
+python manage.py collectstatic --noinput  # Ensure noinput is used to bypass prompts
 python manage.py tailwind start
 
 echo "BUILD COMPLETE"
