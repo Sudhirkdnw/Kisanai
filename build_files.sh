1962eee8e0d20683a5ad417e52d2ccafd5ba4aac
Sudhir Kumar <sudhirkdnw@gmail.com>
2024-09-13 21:23:59 +0530

    final

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
-
 echo "BUILD START"
 
+# Activate virtual environment
+source venv/bin/activate
+
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
+# Run Django commands
 python manage.py makemigrations
 python manage.py migrate
 python manage.py tailwind install
-python manage.py collectstatic --noinput  # Ensure noinput is used to bypass prompts
+python manage.py collectstatic --noinput
 python manage.py tailwind start
 
-echo "BUILD COMPLETE"
+echo "MIGRATION END"
