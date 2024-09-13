0080623345bd00d6c7d5457f8630de885ff56646
Sudhir Kumar <sudhirkdnw@gmail.com>
2024-09-13 21:01:32 +0530

    sh

--- build_files.sh
+++ build_files.sh
@@ -1,22 +1,19 @@
+#!/bin/bash
+
 echo "BUILD START"
 
-# Activate virtualenv if required, or check Python path
-if command -v python3 &>/dev/null; then
-    PYTHON_CMD=python3
-elif command -v python &>/dev/null; then
-    PYTHON_CMD=python
-else
-    echo "Python is not installed."
-    exit 1
-fi
+# Activate virtual environment
+source venv/bin/activate
 
-# Install necessary packages
-$PYTHON_CMD -m ensurepip --upgrade
-$PYTHON_CMD -m pip install -r requirements.txt
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
 
-# Django specific commands
-$PYTHON_CMD manage.py makemigrations
-$PYTHON_CMD manage.py migrate
-$PYTHON_CMD manage.py collectstatic --noinput
+# Run Django commands
+python manage.py makemigrations
+python manage.py migrate
+python manage.py tailwind install
+python manage.py collectstatic --noinput
+python manage.py tailwind start
 
-echo "BUILD COMPLETE"
+echo "MIGRATION END"
