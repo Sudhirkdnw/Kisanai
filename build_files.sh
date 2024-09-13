a496b068f63a24a7cba3686b0fa8be18c979b45c
Sudhir Kumar <sudhirkdnw@gmail.com>
2024-09-13 19:57:08 +0530

    shsh

--- build_files.sh
+++ build_files.sh
@@ -4,8 +4,6 @@ pip install -r requirements.txt
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py tailwind install
-python manage.py collectstatic 
-python manage.py tailwind start
+python manage.py collectstatic --no-input  # Avoid prompts during build
 
-echo "MIGRATION END"
+echo "BUILD COMPLETE"
